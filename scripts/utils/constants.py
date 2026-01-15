7dc183a11c8a0ea1b727d5cbbd47dc365bfd81b6
anjahess <anja.hess@mail.de>
2026-01-15 16:36:46 +0100

    add constant for utils directory

--- scripts/utils/constants.py
+++ scripts/utils/constants.py
@@ -11,7 +11,9 @@ import os
 
 script_dir = os.path.dirname(__file__)
 PACKAGE_DIR = script_dir.rsplit('scripts/utils')[0]
-SOUCE_DATA_DIR = f"{PACKAGE_DIR}/sourcedata/"
-RESULTS_DIR = f"{PACKAGE_DIR}/results/"
+SOURCE_DATA_DIR = f"{PACKAGE_DIR}/sourcedata/"
+UTILS_DIR = f"{PACKAGE_DIR}/scripts/utils/"
+
+RESULTS_DIR = f"{PACKAGE_DIR}results/"
 FIGURE_DIR = f"{RESULTS_DIR}figures/"
 TABLE_DIR = f"{RESULTS_DIR}tables/"
